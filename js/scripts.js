73d4f2edcc831fb02cba7f6309a3f14117febc5f
Donovan Weber <donovanweber03@gmail.com>
2022-06-03 13:59:01 -0400

    Add values to options and css to display results.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,17 @@ $(document).ready(function() {
   $("form#favoriteAnimal").submit(function(event) {
     const favoriteAnimal = $("select#favoriteAnimal").val();
     const favoriteMusic = $("select#favoriteMusic");
+    const favoriteElf = $("inpu:radio[name=elf]:checked").val();
   let result;
   if (favoriteAnimal === "Dogs") {
     result = $("#dogs").show();
   } else if (favoriteAnimal === "Cats") {
     result = $("#cats").show();
+  } else if (favoriteAnimal === "Snakes") {
+    result = $("#snakes").show();
   }
-   
+   $("#outputResult").show(result);
   
   });
 });
+
