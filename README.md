a1e3cc2a16115a723fbc18d98b92e9dca5716e76
Donovan Weber <donovanweber03@gmail.com>
2022-06-03 17:24:48 -0400

    Add README BUGFIX:JS answes not properly showing.

---
+++ README.md
@@ -0,0 +1,31 @@
+# _Promgramming Language Suggester_
+
+#### By Donovan Weber
+
+#### Portfolio of my work at Epicodus and About Me
+
+## Technologies Used
+
+* HTML
+* CSS
+* JavaScript
+
+
+## Description
+
+This Webpage shows my abilties with CSS and HTML by producing a portfolio blog of personal topics such as my background, hobbies, and general information about me. It also highlights a few of my projects that are in development with Epicodus
+
+## Setup/Installation Requirements
+
+* Clone my project-portfolio repository from Github
+* Go to the first level of the directory
+* Open portfolio-project/index.html
+
+## Known Bugs
+
+No known bugs contact me at [donovanweber03@gmail.com](mailto:donovanweber03@gmail.com) if any bugs are 
+found.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,18 @@ $(document).ready(function() {
   $("form#favoriteAnimal").submit(function(event) {
     event.preventDefault();
     const favoriteAnimal = $("select#favoriteAnimal").val();
-    const favoriteMusic = $("select#favoriteMusic");
-    const favoriteElf = $("input:radio[name=elf]:checked").val();
-    let result;
+    // const favoriteMusic = $("select#favoriteMusic");
+    // const favoriteElf = $("input:radio[name=elf]:checked").val();
+   
   if (favoriteAnimal === "dogs") {
     $("#resultDogs").show();
+    $("#resultDogs").hide();
   } else if (favoriteAnimal === "cats") {
     $("#resultCats").show();
+    $("#resultCats").hide();
   } else (favoriteAnimal === "snakes") 
     $("#resultSnakes").show();
-  
-  $("#outputResult").show(result);
+    $("#resultSnakes").hide(); 
   
   });
 });
